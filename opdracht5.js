1300cf2ee779dd4a66b7475b2f7e2def619cd59a
EmileDeGamer <emilemolzoho@gmail.com>
2019-10-29 10:28:00 +0100

    changes

---
+++ opdracht5.js
@@ -0,0 +1,20 @@
+let number = 1, newnumber = 1, answer = 1
+
+function Calculate(){
+
+    for (let i = 0; i <= 20; i++) {
+        
+        number
+
+        //newnumber = answer + newnumber
+        //answer = number + newnumber
+        //number = answer
+        //answer = answer + newnumber
+        //answer = answer + number
+        //number = number + answer
+        //newnumber = number + newnumber
+        console.log(answer)
+        //console.log(number)
+    }
+    
+}
